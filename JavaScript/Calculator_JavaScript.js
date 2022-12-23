639573dbce2a06e85ce4e6bc3c1037a47a1deea6
Mahrukh Ejaz <bobo.zh.cai@gmail.com>
2022-12-23 23:01:31 +0500

    done
    done

---
+++ JavaScript/Calculator_JavaScript.js
@@ -0,0 +1,115 @@
+// basic calculator object for default values
+const Calculator = {
+    Display_Value: '0',
+    First_Operand: null,
+    Wait_Second_Operand: false,
+    operator: null,
+};
+
+// function to take the input from the user
+
+function Input_Digit(digit) {
+    const { Display_Value, Wait_Second_Operand } = Calculator;
+
+    if (Wait_Second_Operand === true) {
+        Calculator.Display_Value = digit;
+        Calculator.Wait_Second_Operand = false;
+    }
+    else {
+        Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+    }
+}
+
+// function to take floating number input
+function Input_Decimal(dot) {
+    if (Calculator.Wait_Second_Operand === true) return;
+    if (!Calculator.Display_Value.includes(dot)) {
+        Calculator.Display_Value += dot;
+    }
+}
+
+// function to add the second number
+function Handle_Operator(Next_Operator) {
+    const { First_Operand, Display_Value, operator } = Calculator;
+
+    const Value_of_Input = parseFloat(Display_Value);
+
+    if (operator && Calculator.Wait_Second_Operand) {
+        Calculator.operator = Next_Operator;
+        return;
+    }
+    if (First_Operand == null) {
+        Calculator.First_Operand = Value_of_Input;
+    }
+    else if (operator) {
+        const Value_Now = First_Operand || 0;
+
+        let result = Perform_Calculation[operator](Value_Now, Value_of_Input);
+
+        result = Number(result).toFixed(9)
+
+        result = (result * 1).toString();
+        Calculator.Display_Value = parseFloat(result);
+        Calculator.First_Operand = parseFloat(result);
+    }
+    Calculator.Wait_Second_Operand = true
+    Calculator.operator = Next_Operator;
+}
+
+// object for operations
+const Perform_Calculation = {
+    '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
+    '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
+    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    '=': (First_Operand, Second_Operand) => Second_Operand
+};
+
+// function to rest calculator
+function Calculator_Reset() {
+    Calculator.Display_Value = '0';
+    Calculator.First_Operand = null;
+    Calculator.Wait_Second_Operand = false;
+    Calculator.operator = null;
+}
+
+// function to display the latest 
+
+function Update_Display() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = Calculator.Display_Value;
+}
+
+Update_Display();
+
+const keys = document.querySelector('.calculator-keys');
+
+// event listener of click on the button
+keys.addEventListener('click', (event) => {
+    // console.log("pressed")
+    const { target } = event;
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        Handle_Operator(target.value);
+        Update_Display();
+        return;
+    }
+    if (target.classList.contains('decimal')) {
+        Input_Decimal(target.value);
+        Update_Display();
+        return;
+    }
+
+    if (target.classList.contains("all-clear")) {
+        Calculator_Reset();
+        Update_Display();
+        return;
+    }
+
+    console.log("pressed", target.value)
+
+    Input_Digit(target.value);
+    Update_Display();
+})
---
+++ JavaScript_3.js
@@ -0,0 +1,5 @@
+
+function displayCategory(game){
+    var gameType = game.getAttribute("data-game-type");
+    alert(gameType + " is the game type of " + game.innerHTML)
+}
